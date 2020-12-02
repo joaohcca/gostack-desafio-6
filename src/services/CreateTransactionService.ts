c001a68e1c1e0da063b0855d208ad9013db7db25
Joao Albuquerque <joaohcca@gmail.com>
2020-12-01 23:24:47 -0300

    adicionando resolucao do desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,64 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import TransactionRepository from '../repositories/TransactionsRepository'
+
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+import { json } from 'express';
+
+interface Request {
+  title: 'string',
+  type: 'income' | 'outcome',
+  value: number,
+  category: string
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository)
+    const categoryRepository = getRepository(Category)
+
+    const { total } = await transactionRepository.getBalance();
+
+    if ( type === 'outcome' && total < value ) {
+        throw new AppError('Inssuficient Balance to complete')
+    }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category
+      }
+    })
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category
+      })
+
+      await categoryRepository.save(transactionCategory)
+    }
+      
+    
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+      category_id: transactionCategory.id
+    })
+
+    await transactionRepository.save(transaction)
+
+    return transaction
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,9 +1,88 @@
+import csvParse from 'csv-parse';
+import { response } from 'express';
+import fs from 'fs';
+import { In, getRepository, getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
+import Category from '../models/Category'
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string
+}
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionsRepository)
+    const categoryRepository = getRepository(Category)
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parser = csvParse({
+      from_line: 2,
+    }) 
+
+    const parseCSV = contactsReadStream.pipe(parser)
+
+    const transactions: CSVTransaction[]=[];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+      cell.trim(),
+      );
+
+      if (!title ||!type || !value) return
+
+      categories.push(category);
+      transactions.push({title, type, value, category});
+    });
+    await new Promise(resolve => parseCSV.on('end', resolve))
+
+    const currentCategories = await categoryRepository.find({
+      where: {
+        title: In(categories)
+      }
+    })
+
+    const currentCategoriesTitles = currentCategories.map(
+      (category: Category) => category.title);
+
+    const addCategoriesTitles = categories
+    .filter(category => !currentCategoriesTitles.includes(category))
+    .filter((value, index, self) => self.indexOf(value)=== index) 
+
+    const newCategories = categoryRepository.create(
+      addCategoriesTitles.map(title => ({
+        title
+      }))
+    )
+
+    await categoryRepository.save(newCategories)
+
+    const finalCategories = [... newCategories, ...currentCategories];
+
+    const createdTransactions = transactionRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category
+        ),
+      }))
+    )
+    
+    await transactionRepository.save(createdTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return createdTransactions
+    //return {transactions, categories}
   }
+  
 }
 
 export default ImportTransactionsService;
